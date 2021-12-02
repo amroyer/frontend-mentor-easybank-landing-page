85e26b4e82ffa0d28fade1116c9c22f883bfec6c
Aaron Royer <amroyerdev@gmail.com>
2021-12-02 09:53:02 -0700

    finished dropdown menu

--- dist/script.js
+++ dist/script.js
@@ -12,3 +12,22 @@ const handleScroll = (e) => {
 };
 
 document.addEventListener("scroll", handleScroll);
+
+const overlay = document.querySelector(".overlay");
+const dropdownNav = document.querySelector(".dropdown__nav");
+const introImage = document.querySelector(".intro__image");
+const headerIcon = document.querySelector(".header__icon");
+const hamburger = document.querySelector(".header__hamburger");
+const cross = document.querySelector(".header__cross");
+
+const handleIconClick = () => {
+  hamburger.classList.toggle("invisible");
+  hamburger.classList.toggle("rotated");
+  cross.classList.toggle("invisible");
+  cross.classList.toggle("rotated");
+  overlay.classList.toggle("invisible");
+  introImage.classList.toggle("invisible");
+  dropdownNav.classList.toggle("hidden");
+};
+
+headerIcon.addEventListener("click", handleIconClick);
